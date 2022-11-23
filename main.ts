2a130c61af21000b26ade3125d259bf75617e167
DGarayP <david10559@hotmail.com>
2022-11-23 15:36:11 -0500

    Deber Aplicaciones Moviles

---
+++ main.ts
@@ -0,0 +1,12 @@
+let total=function(input:number[]):number{
+    let sum:number=0;
+    for(let i=0;i<input.length;i++){
+        if(isNaN(input[i])){
+            continue;
+        }
+        sum=sum+input[i];
+    }
+    return sum;
+}
+
+console.log(total([2,4,8]))
