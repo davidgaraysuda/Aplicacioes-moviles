8fc33c19f59c681e63e7929417338cfd6163b2d1
DGarayP <david10559@hotmail.com>
2022-11-28 20:10:49 -0500

    Deber funcion flecha

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-let total=function(input:number[]):number{
+/*let total=function(input:number[]):number{
     let sum:number=0;
     for(let i=0;i<input.length;i++){
         if(isNaN(input[i])){
@@ -9,4 +9,10 @@ let total=function(input:number[]):number{
     return sum;
 }
 
-console.log(total([2,4,8]))
+console.log(total([2,4,8]))*/
+
+let displayMessage= (message:string)=>
+    "The message is" + " " + message;
+
+
+console.log(displayMessage("HOLA"));
